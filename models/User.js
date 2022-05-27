a86259fb4b2de515bd4decdc9765eca8c6c8cf1c
Rahul <rahulvaidya148@gmail.com>
2022-05-27 13:27:52 +0530

    Added Signup Successfully

---
+++ models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    cpassword: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    view: {
+        type: String,
+        default: 'daily'
+    }
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+//---------User model----------//
+const User = require('../models/User')
 
 //---------Register Page----------//
 router.get('/register', (req, res) => res.render('register'));
@@ -8,6 +10,74 @@ router.get('/register', (req, res) => res.render('register'));
 
 //---------Login Page----------//
 router.get('/login', (req, res) => res.render('login'));
+
+
+
+//---------Register Handle----------//
+router.post('/register',(req,res) => {
+    const {name , email , password ,cpassword} = req.body;
+    
+    //---------Checking for errors----------//
+    let errors = [];
+
+    if (!name || !email || !password ||!cpassword) {
+        errors.push({ msg: 'Please enter all fields' });
+    }
+      if (password != cpassword){
+        errors.push({ msg: 'Password not matched!!' });
+
+        return res.redirect('back');
+    }
+
+    if (errors.length > 0) {
+        res.render('register', {
+            errors,
+            name,
+            email,
+            password,
+            cpassword
+            
+        });
+    }else {
+        // --------------Validation passed----------->//
+        User.findOne({email : email}).then (user => {
+            if(user){
+                //---------User already exists----------//
+                errors.push({ msg: 'Email ID already exists' });
+                res.render('register', {
+                    errors,
+                    name,
+                    email,
+                    password,
+                    cpassword
+                });
+            }else{
+                const newUser = new User({
+                    name,
+                    email,
+                    password,
+                    cpassword
+                });
+                 
+            //---------Save user----------//
+            newUser
+            .save()
+            .then(user => {
+                req.flash(
+                'success_msg',
+                'You are now registered and can log in'
+                );
+                res.redirect('/users/login');
+            }) 
+            .catch(err => console.log(err));
+           
+            }
+        });
+    }
+});
+
+
+
 
 
 module.exports = router;
